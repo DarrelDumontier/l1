3f780a81d73eb138af9d9f6f8de177f24c177a1f
Darrel Dumontier <darrel.dumontier@outlook.com>
2026-03-28 11:04:14 -0500

    changed back to div
    using divs is more appropriate here, since figure, and caption have specific meanings that  makes no sense for a QR code.

--- app/qr/qr.tsx
+++ app/qr/qr.tsx
@@ -4,10 +4,10 @@ import qrImage from './image-qr-code.png'
 
 export default function QR() {
 return (
-<figure className='w-qrwidth p-5 rounded-4xl text-[15px] bg-theme-white space-y-4 pb-8'>
-    <img src={qrImage} alt="QR code" className='rounded-lg'/>
-    <h1 className='text-center text-theme-slate3 font-bold text-[1.4em]'>Improve your front-end skills by building projects</h1>
-    <figcaption className='text-center text-theme-slate3'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</figcaption>
-</figure>
+<div className='w-qrwidth p-5 rounded-4xl text-[15px] bg-theme-white space-y-4 pb-8'>
+    <img src={qrImage} alt="Fake QR code for Frontend Mentor assignment" className='rounded-lg'/>
+    <p className='text-center text-theme-slate3 font-bold text-[1.4em]'>Improve your front-end skills by building projects</p>
+    <p className='text-center text-theme-slate3'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+</div>
 )
 }
